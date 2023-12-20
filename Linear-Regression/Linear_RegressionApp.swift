9b83615051756464aa2ec46e4d359e3af4608aea
Ress1206-bit <adam.ress@icloud.com>
2023-12-19 20:38:49 -0500

    Initial Commit

---
+++ Linear-Regression/Linear_RegressionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Linear_RegressionApp.swift
+//  Linear-Regression
+//
+//  Created by Adam Ress on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Linear_RegressionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
